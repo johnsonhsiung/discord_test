6006e3de310cd053bc72d4dc92618f2d3ffa7f5a
Johnson Hsiung <johnson.hsiung@sjsu.edu>
2024-06-17 14:37:37 -0700

    Rather than replying to messages, the bot know listens for commands with '!'

--- bot.py
+++ bot.py
@@ -1,51 +1,88 @@
+
 # bot.py
 import os
-
-import discord
+import random
 from dotenv import load_dotenv
+
+# 1
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.members = True # see members of the server
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-intents = discord.Intents.default()
-intents.members = True
-client = discord.Client(intents=intents)
+# 2
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
-    print(f'{client.user.name} has connected to Discord to {guild}')
+    guild = discord.utils.find(lambda g: g.name == GUILD, bot.guilds)
+    print(f'{bot.user.name} has connected to Discord to {guild}')
     members = '\n - '.join([member.name for member in guild.members])
-    # print(f'Guild Members:\n - {members}')
+    print(f'Guild Members:\n - {members}')
+
+
+@bot.command(name='quack')
+async def quack(ctx):
+    response = 'QUACK'
+    await ctx.send(response)
+
+bot.run(TOKEN)
+
+
+# bot.py
+# import os
+
+# import discord
+# from dotenv import load_dotenv
+
+# load_dotenv()
+# TOKEN = os.getenv('DISCORD_TOKEN')
+# GUILD = os.getenv('DISCORD_GUILD')
+
+# intents = discord.Intents.default()
+# intents.members = True
+# client = discord.Client(intents=intents)
+
+# @client.event
+# async def on_ready():
+#     guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+#     print(f'{client.user.name} has connected to Discord to {guild}')
+#     members = '\n - '.join([member.name for member in guild.members])
+#     # print(f'Guild Members:\n - {members}')
     
 
 
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to {member.guild.name}! Say "hi" in Recruit\'s Pond to get some Quacks!'
-    )
+# @client.event
+# async def on_member_join(member):
+#     await member.create_dm()
+#     await member.dm_channel.send(
+#         f'Hi {member.name}, welcome to {member.guild.name}! Say "hi" in Recruit\'s Pond to get some Quacks!'
+#     )
 
-@client.event
-async def on_message(message):
-    # ignore messages sent by this bot 
-    if message.author == client.user:
-        return 
-    if 'hi' in message.content.lower():
-        await message.channel.send("Quack")
-    elif message.content == 'raise-exception':
-        raise discord.DiscordException
+# @client.event
+# async def on_message(message):
+#     # ignore messages sent by this bot 
+#     if message.author == client.user:
+#         return 
+#     if 'hi' in message.content.lower():
+#         await message.channel.send("Quack")
+#     elif message.content == 'raise-exception':
+#         raise discord.DiscordException
 
-@client.event
-async def on_error(event, *args, **kwargs):
-    with open('err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Exception message: {args[0]}\n')
-        else:
-            raise
+# @client.event
+# async def on_error(event, *args, **kwargs):
+#     with open('err.log', 'a') as f:
+#         if event == 'on_message':
+#             f.write(f'Exception message: {args[0]}\n')
+#         else:
+#             raise
 
 
 
-client.run(TOKEN)
+# client.run(TOKEN)
+
