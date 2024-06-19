42586ad11a40c43945de5b54b4743c6faa731267
Johnson Hsiung <johnson.hsiung@sjsu.edu>
2024-06-18 18:46:19 -0700

    added woof and going over the character limit per message

--- bot.py
+++ bot.py
@@ -8,6 +8,8 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 
+CHARACTER_LIMIT = 2000
+
 intents = discord.Intents.default()
 intents.members = True # see members of the server
 
@@ -18,6 +20,19 @@ GUILD = os.getenv('DISCORD_GUILD')
 # 2
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+
+async def send_message(ctx, response, reps):
+   
+    responses_per_message = CHARACTER_LIMIT // (len(response) + 1)
+    messages = reps // responses_per_message
+
+    for _ in range(messages):
+        print(messages, responses_per_message)
+        await ctx.send(' '.join([response for _ in range(responses_per_message)])) 
+
+    await ctx.send(' '.join([response for _ in range(reps % responses_per_message)])) 
+
+
 @bot.event
 async def on_ready():
     guild = discord.utils.find(lambda g: g.name == GUILD, bot.guilds)
@@ -27,15 +42,23 @@ async def on_ready():
 
 
 @bot.command(name='quack', help='Responds with QUACK')
-async def quack(ctx, reps: int=1):
+async def quack(ctx, reps: int):
     response = 'QUACK'
-    await ctx.send(response)
+    await send_message(ctx, response, reps)
+
+@bot.command(name='woof', help='Responds with WOOF')
+async def woof(ctx, reps: int):
+    response = 'WOOF'
+    await send_message(ctx, response, reps)
 
 @bot.command(name='quack_rep', help='QUACK with repetitions')
 @commands.has_permissions(administrator=True)
 async def quack_rep(ctx, reps: int):
+
     response = 'QUACK'
-    await ctx.send(' '.join([response for _ in range(reps)])) 
+
+
+
 
 @bot.event
 async def on_member_join(member):
